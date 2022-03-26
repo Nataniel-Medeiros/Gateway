9670b7544c171668b449bb980cb52364b63ec8df
Nataniel Medeiros <j.nataniel.medeiros@gmail.com>
2022-03-26 12:59:08 -0400

    Alguns comentários no index.js

--- Gateway/index.js
+++ Gateway/index.js
@@ -1,10 +1,15 @@
+//Preparação das libs.
 const httpProxy = require('express-http-proxy');
 const express = require('express');
-const app = express();
 var logger = require('morgan');
- 
+
+//Iniciando app.
+const app = express();
+
+//Log para depuração em nível de desenvolvimento.
 app.use(logger('dev'));
  
+//Selecação do micro serviço.
 function selectProxyHost(req) {
     if (req.path.startsWith('/serviceA'))
         return 'http://localhost:8091/';
@@ -12,10 +17,12 @@ function selectProxyHost(req) {
         return 'http://localhost:8092/';
 }
  
+//Requisição do microserviço.
 app.use((req, res, next) => {
     httpProxy(selectProxyHost(req))(req, res, next);
 });
- 
+
+//Subindo seviço.
 app.listen(3000, () => {
     console.log('API Gateway running!');
 });
